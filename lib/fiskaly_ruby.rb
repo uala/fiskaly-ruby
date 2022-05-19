ea728658711b2dd6117a326af034afc530a31ab5
pietro ventimiglia <twntymls@gmail.com>
2022-05-19 15:21:41 +0200

    fixed rubocop target ruby version and generated rubocop todo file

--- lib/fiskaly_ruby.rb
+++ lib/fiskaly_ruby.rb
@@ -1,7 +1,7 @@
 module FiskalyRuby
-	VERSION = '0.1.0'
+  VERSION = '0.1.0'
 
-	def self.management_authenticate(args = {})
-		FiskalyService::Management::Authenticate.call(api_key: args.fetch(:api_key), api_secret: args.fetch(:api_secret))
-	end
+  def self.management_authenticate(args = {})
+    FiskalyService::Management::Authenticate.call(api_key: args.fetch(:api_key), api_secret: args.fetch(:api_secret))
+  end
 end
