37327da81dd30ea39dbe3f5fc4116063f8b4b8ef
Joseph Schito <joseph.schito@gmail.com>
2022-05-24 14:16:08 +0200

    Refactor spec

--- spec/fiskaly-ruby_spec.rb
+++ spec/fiskaly-ruby_spec.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 RSpec.describe FiskalyRuby do
-  it 'has a version number' do
-    expect(FiskalyRuby::VERSION).not_to be nil
+  describe 'VERSION' do
+    it 'has a version number' do
+      expect(FiskalyRuby::VERSION).not_to be nil
+    end
   end
 end
