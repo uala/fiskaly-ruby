1b38d6aa4ec33d43ae9420661f4d7290813b9088
Joseph Schito <joseph.schito@gmail.com>
2022-05-27 15:02:24 +0200

    Test `COMMANDS` to methods

--- spec/fiskaly-ruby_spec.rb
+++ spec/fiskaly-ruby_spec.rb
@@ -1,9 +1,33 @@
 # frozen_string_literal: true
 
 RSpec.describe FiskalyRuby do
+  before do
+    allow(FiskalyRuby).to receive(:call).and_return(nil)
+  end
+
   describe 'VERSION' do
     it 'has a version number' do
-      expect(FiskalyRuby::VERSION).not_to be nil
+      expect(described_class::VERSION).not_to be nil
+    end
+  end
+
+  describe 'COMMANDS' do
+    described_class::COMMANDS.each do |command|
+      method_name = command.to_method_name.to_sym
+
+      describe ".#{method_name}" do
+        let(:args) { { some: { random: :arguments } } }
+
+        before do
+          allow(command).to receive(:call).with(args)
+
+          described_class.send(method_name, args)
+        end
+
+        it "should run #{command}#call and pass the same args" do
+          expect(command).to have_received(:call).with(args)
+        end
+      end
     end
   end
 end
