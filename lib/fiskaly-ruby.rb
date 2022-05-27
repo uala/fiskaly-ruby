b51be6d2a87cf85dcddd9087956c11a05bfeb196
Joseph Schito <joseph.schito@gmail.com>
2022-05-27 15:02:03 +0200

    Add `Base.to_method_name` method

--- lib/fiskaly-ruby.rb
+++ lib/fiskaly-ruby.rb
@@ -43,16 +43,7 @@ module FiskalyRuby
 
   class << self
     COMMANDS.each do |command|
-      name = command.name
-      method_name = name.split('::')[1..].map do |command_name|
-        if %w(KassenSichV TSS DSFinVK).include?(command_name)
-          command_name.downcase
-        else
-          command_name.gsub(/(.)([A-Z])/, '\1_\2').downcase
-        end
-      end.join('_')
-
-      define_method(method_name) do |args|
+      define_method(command.to_method_name) do |args|
         command.call(args)
       end
     end
